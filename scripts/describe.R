5f56c15f167f961b43aac7c660314f126052a6c3
Felipe Figueiredo <philsf79@gmail.com>
2021-06-15 17:16:27 -0300

    EDA

--- scripts/describe.R
+++ scripts/describe.R
@@ -1,12 +1,33 @@
 # setup -------------------------------------------------------------------
 
 # library(Hmisc) # describe
-# library(skimr) # skim
+library(skimr) # skim
 # library(tableone)
 # library(gmodels) # CrossTable
 
 # exploratory -------------------------------------------------------------
 
+dados %>% skim()
+
+# categoricas
+
+dados %>% count(sexo)
+dados %>% count(escolaridade)
+dados %>% count(renda)
+dados %>% count(ano_atq)
+dados %>% count(aposentado)
+dados %>% count(habitos_de_vida)
+dados %>% count(cirurgia_durante_a_espera)
+dados %>% count(deambulacao)
+dados %>% count(medicacoes_em_uso)
+dados %>% count(uso_de_analgesicos)
+dados %>% count(anti_depressivos)
+dados %>% count(motivo_da_atq)
+
+# numericas
+
+dados %>%
+  select(idade_num, tempo_de_espera, charlson, hhs) %>% skim()
 
 # tables ------------------------------------------------------------------
 

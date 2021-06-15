a70af0a1b6b44bd860add87ffc04925aefb3ba53
Felipe Figueiredo <philsf79@gmail.com>
2021-06-15 16:43:06 -0300

    input básico

--- scripts/input.R
+++ scripts/input.R
@@ -1,19 +1,40 @@
 # setup -------------------------------------------------------------------
 library(readxl)
-library(data.table)
-# library(tidyverse)
-
+# library(data.table)
+library(tidyverse)
+library(janitor)
 
 # data loading ------------------------------------------------------------
 # raw.data <- fread("dataset/file.csv")
 # raw.data <- readxl::read_excel("dataset/file.xls")
 
 # raw.data <- data.table(raw.data)
+dados.raw <- read_excel("dataset/PLANILHA UNIFORMIZADA.xlsx")
+dados.raw <- dados.raw %>%
+  clean_names()
 
 # data cleaning -----------------------------------------------------------
 
+dados <- dados.raw %>%
+  # elimina colunas que não serão usadas
+  select(-(acetabulo:obs))
 
+dados <- dados %>%
+  # limpeza
+  mutate(
+    id = as.character(id),
+    aposentado = str_to_title(aposentado),
+    causa = str_to_title(causa),
+    cirurgia_durante_a_espera = str_to_title(cirurgia_durante_a_espera),
+    anti_depressivos = str_to_title(anti_depressivos),
+    # contralateral = str_to_title(contralateral),
+  )
 
 # data wrangling ----------------------------------------------------------
 
+dados <- dados %>%
+  # idade_num ao lado de idade para comparação
+  mutate(idade_num = parse_number(idade), .after = idade) %>%
+  # preenche todos os NA em causa (assume que todos os NA são ativos)
+  replace_na(list(causa = "Trabalhando"))
 

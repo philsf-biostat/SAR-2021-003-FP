fdc624bdfa0e9fb5a554a5c31acc1c5c9f194ae1
Felipe Figueiredo <philsf79@gmail.com>
2021-06-22 18:04:44 -0300

    EDA para apresentação

--- scripts/describe.R
+++ scripts/describe.R
@@ -26,6 +26,10 @@ dados %>% count(uso_de_analgesicos)
 dados %>% count(anti_depressivos)
 dados %>% count(motivo_da_atq)
 
+# EDA ---------------------------------------------------------------------
+
+dados %>% ggplot(aes(tabagismo)) + geom_bar()
+dados %>% ggplot(aes(etilismo)) + geom_bar()
 dados %>% ggplot(aes(uso_de_analgesicos)) + geom_bar()
 dados %>% ggplot(aes(medicacoes_em_uso)) + geom_bar()
 
@@ -39,10 +43,24 @@ dados %>% ggplot(aes(idade_num)) + geom_histogram(binwidth = 10)
 dados %>% ggplot(aes(hhs)) + geom_histogram(binwidth = 10)
 dados %>% ggplot(aes(charlson)) + geom_histogram(binwidth = 2.5) #+ scale_x_log10()
 
-# correlacoes
+# correlacoes entre covariáveis
 dados %>% ggplot(aes(medicacoes_em_uso, charlson)) + geom_jitter(alpha = .5)
 dados %>% ggplot(aes(uso_de_analgesicos, charlson)) + geom_boxplot() + geom_jitter(alpha = .3)
 dados %>% ggplot(aes(hhs, charlson)) + geom_jitter(alpha = .4)
+
+# correlacoes com tempo de espera
+dados %>% ggplot(aes(idade_num, tempo_de_espera)) + geom_jitter(alpha = .5, height = .1, width = .1)
+dados %>% ggplot(aes(sexo, tempo_de_espera)) + geom_jitter(alpha = .5, height = .1, width = .1) + geom_boxplot(alpha = 0)
+dados %>% ggplot(aes(aposentado, tempo_de_espera)) + geom_jitter(alpha = .5, height = .1, width = .1) + geom_boxplot(alpha = 0)
+dados %>% ggplot(aes(factor(tabagismo), tempo_de_espera)) + geom_jitter(alpha = .5, height = .1, width = .1) + geom_boxplot(alpha = 0)
+dados %>% ggplot(aes(factor(etilismo), tempo_de_espera)) + geom_jitter(alpha = .5, height = .1, width = .1) + geom_boxplot(alpha = 0)
+
+dados %>% ggplot(aes(charlson, tempo_de_espera)) + geom_jitter(alpha = .5)
+dados %>% ggplot(aes(hhs, tempo_de_espera)) + geom_jitter(alpha = .5)
+dados %>% ggplot(aes(factor(medicacoes_em_uso), tempo_de_espera)) + geom_jitter(alpha = .5, height = .1, width = .1) + geom_boxplot(alpha = 0)
+dados %>% ggplot(aes(uso_de_analgesicos, tempo_de_espera)) + geom_jitter(alpha = .5, height = .1, width = .1) + geom_boxplot(alpha = 0)
+
+
 
 # tables ------------------------------------------------------------------
 

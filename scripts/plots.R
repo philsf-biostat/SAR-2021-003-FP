ac3fd681f9461daa7e4e38a5b0ffacc8debaac97
Felipe Figueiredo <philsf79@gmail.com>
2021-07-14 16:49:06 -0300

    tempo: honrar o label da var

--- scripts/plots.R
+++ scripts/plots.R
@@ -8,7 +8,7 @@ gg_tempo_anos <-
   dados %>%
   ggplot(aes(tempo_anos)) +
   geom_histogram(binwidth = 1) +
-  xlab("Tempo de espera (anos)") + ylab("n") +
+  xlab(var_label(dados$tempo_anos)) + ylab("n") +
   ylim(c(0, 15)) +
   theme_classic()
 
